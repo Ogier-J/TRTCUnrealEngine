20856001d584f8e88136d9f7ad90f909235b3861
gavinwjwang <gavinwjwang@tencent.com>
2021-11-25 10:21:45 +0800

    xcode for ios

---
+++ TXLiteAVTestUserSig/TXLiteAVTestUserSig/TXLiteAVTestUserSig.h
@@ -0,0 +1,18 @@
+//
+//  TXLiteAVTestUserSig.h
+//  TXLiteAVTestUserSig
+//
+//  Created by gavinwjwang on 2021/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TXLiteAVTestUserSig.
+FOUNDATION_EXPORT double TXLiteAVTestUserSigVersionNumber;
+
+//! Project version string for TXLiteAVTestUserSig.
+FOUNDATION_EXPORT const unsigned char TXLiteAVTestUserSigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TXLiteAVTestUserSig/PublicHeader.h>
+
+
