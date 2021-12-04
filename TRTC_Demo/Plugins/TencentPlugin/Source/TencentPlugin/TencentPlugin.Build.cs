611b5e3f242ee056769d1d768c3d465ac76c51a9
gavinwjwang <gavinwjwang@tencent.com>
2021-12-04 09:54:36 +0800

    改为plugin方式

--- TRTC_Demo/Plugins/TencentPlugin/Source/TencentPlugin/TencentPlugin.Build.cs
+++ TRTC_Demo/Plugins/TencentPlugin/Source/TencentPlugin/TencentPlugin.Build.cs
@@ -1,9 +1,23 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
-
+using System.IO;
 using UnrealBuildTool;
 
 public class TencentPlugin : ModuleRules
 {
+	private string _TRTCSDKPath
+	{
+			get
+			{
+					return Path.GetFullPath(Path.Combine(ModuleDirectory, "TRTCSDK"));
+			}
+	}
+	// private string _DebugPath
+	// {
+	// 		get
+	// 		{
+	// 				return Path.GetFullPath(Path.Combine(ModuleDirectory, "debug"));
+	// 		}
+	// }
 	public TencentPlugin(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
@@ -49,5 +63,96 @@ public class TencentPlugin : ModuleRules
 				// ... add any modules that your module loads dynamically here ...
 			}
 			);
+
+
+		bEnableUndefinedIdentifierWarnings = false;
+
+		if (Target.Platform == UnrealTargetPlatform.Android)
+		{
+				
+		}else if (Target.Platform == UnrealTargetPlatform.IOS)
+		{
+				// 添加插件的包含路径
+				PublicIncludePaths.Add(Path.Combine(_TRTCSDKPath, "include/iOS"));
+				// PublicIncludePaths.Add(Path.Combine(_DebugPath, "include"));
+				PublicAdditionalLibraries.AddRange(new string[] {
+						"resolv",
+						"z",
+						"c++",
+				});
+				PublicFrameworks.AddRange(
+						new string[] {
+								"CoreML",
+								"VideoToolbox",
+								"Accelerate",
+								"CFNetwork",
+								"OpenGLES",
+								"AVFoundation",
+								"CoreTelephony"
+						}
+				);
+				// PublicAdditionalFrameworks.Add(new Framework( "TXLiteAVSDK_TRTC",Path.Combine(_TRTCSDKPath, "iOS","TXLiteAVSDK_TRTC.framework.zip"), "", true));
+				PublicAdditionalFrameworks.Add(new UEBuildFramework( "TXLiteAVSDK_TRTC",_TRTCSDKPath+"/ios/TXLiteAVSDK_TRTC.framework.zip", ""));
+				//PublicAdditionalLibraries.Add(Path.Combine(_DebugPath, "ios", "libTXLiteAVTestUserSig.a"));
+
+				//PublicAdditionalFrameworks.Add(new UEBuildFramework( "TXLiteAVTestUserSig",_DebugPath+"/ios/TXLiteAVTestUserSig.framework.zip", ""));
+		}else if(Target.Platform == UnrealTargetPlatform.Mac)
+		{
+				// 添加插件的包含路径
+				PublicIncludePaths.Add(Path.Combine(_TRTCSDKPath, "include/Mac"));
+				// PublicIncludePaths.Add(Path.Combine(_DebugPath, "include"));
+				PublicAdditionalLibraries.AddRange(new string[] {
+						"resolv",
+						"z",
+						"c++",
+						"bz2",
+				});
+				PublicFrameworks.AddRange(
+						new string[] {
+								"AppKit",
+								"IOKit",
+								"CoreVideo",
+								"CFNetwork",
+								"OpenGl",
+								"CoreGraphics",
+								"Accelerate",
+								"CoreFoundation",
+								"SystemConfiguration",
+								"AudioToolbox",
+								"VideoToolbox",
+								"CoreTelephony",
+								"CoreWLAN",
+								"AVFoundation",
+								"CoreMedia",
+								"CoreAudio",
+								"AudioUnit",
+								"Accelerate",
+						});
+				PublicFrameworks.Add(Path.Combine(_TRTCSDKPath, "Mac", "Release","TXLiteAVSDK_TRTC_Mac.framework"));
+				//PublicFrameworks.Add(Path.Combine(_DebugPath, "Mac", "Release","TXLiteAVTestUserSig.framework"));
+				
+		}else if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+				//PublicIncludePaths.Add(Path.Combine(_DebugPath, "include"));
+				PublicIncludePaths.Add(Path.Combine(_TRTCSDKPath, "include/win64"));
+				// Add the import library
+				PublicAdditionalLibraries.Add(Path.Combine(_TRTCSDKPath, "win64", "Release","liteav.lib"));
+
+				// Delay-load the DLL, so we can load it from the right place first
+				PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "liteav.dll"));
+				PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "LiteAvAudioHook.dll"));
+				PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "LiteAvAudioHookService.dll"));
+				PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "openh264.dll"));
+				PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "TRAE.dll"));
+
+				////PublicAdditionalLibraries.Add(Path.Combine(_DebugPath, "win64", "Release","zlib.lib"));
+				RuntimeDependencies.Add("$(BinaryOutputDir)/liteav.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "liteav.dll"));
+				RuntimeDependencies.Add("$(BinaryOutputDir)/LiteAvAudioHook.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "LiteAvAudioHook.dll"));
+				RuntimeDependencies.Add("$(BinaryOutputDir)/LiteAvAudioHookService.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "LiteAvAudioHookService.dll"));
+				RuntimeDependencies.Add("$(BinaryOutputDir)/openh264.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "openh264.dll"));
+				RuntimeDependencies.Add("$(BinaryOutputDir)/TRAE.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "TRAE.dll"));
+		}
+
+
 	}
 }
