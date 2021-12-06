47d2f36aeb2979959af9051479e165ca7081dd14
gavinwjwang <gavinwjwang@tencent.com>
2021-12-06 16:08:01 +0800

    界面&debug user sig变更

--- TRTC_Demo/Source/TRTC_Demo/debug/include/DebugDefs.h
+++ TRTC_Demo/Source/TRTC_Demo/debug/include/DebugDefs.h
@@ -6,20 +6,18 @@
 #ifndef QTMACDEMO_BASE_DEFS_H_
 #define QTMACDEMO_BASE_DEFS_H_
 
-/**
- * 仅仅for test 
- */
-static const int testSDKAppID = 1400393897;
-static const char *testUserId = "a9715d2fe80d6dda38db9af2dd7621ad";
-static const char *testUserSig = "eJwtzcsOgjAQBdB-6dqQaUtfJi7UBcTICoyPXckUqESCCsTE*O8ispxzc**8SbZPg8E9yJKwAMhiuj26pvOFn9gaRQWywmlAiWi5xtzYgiEqyajFufPE2ratR7KkIQA3XBv1T9yr9Q83uhCCAcBfO3-7meSaKuBg5hVfji*j0Mg4bS6bbXIVLOrv-aGK6zA7Jucqdvd4vRvkXu1OmJcr8vkChP04bA__";
+// 测试的登录房间ID。
 static const char *testStrRoomId = "110";
+// 测试登录的UserId
+static const char *testUserId = "a9715d2fe80d6dda38db9af2dd7621ad";
+
 /**
  * 腾讯云 SDKAppId，需要替换为您自己账号下的 SDKAppId。
  *
  * 进入腾讯云实时音视频[控制台](https://console.cloud.tencent.com/rav) 创建应用，即可看到 SDKAppId，
  * 它是腾讯云用于区分客户的唯一标识。
  */
-static const int SDKAppID = 1400188366;
+static const int SDKAppID = 0;
 
 /**
  * 计算签名用的加密密钥，获取步骤如下：
@@ -31,7 +29,7 @@ static const int SDKAppID = 1400188366;
  * 注意：该方案仅适用于调试Demo，正式上线前请将 UserSig 计算代码和密钥迁移到您的后台服务器上，以避免加密密钥泄露导致的流量盗用。
  * 文档：https://cloud.tencent.com/document/product/647/17275#Server
  */
-static const char *SECRETKEY = "217a9b4a174649a8a41ea7166faa8666e0973a3312ef9b20ad1ad52e9bbb5e94";
+static const char *SECRETKEY = "";
 
 /**
  *  签名过期时间，建议不要设置的过短
@@ -40,5 +38,12 @@ static const char *SECRETKEY = "217a9b4a174649a8a41ea7166faa8666e0973a3312ef9b20
  *  默认时间：7 x 24 x 60 x 60 = 604800 = 7 天
  */
 static const int EXPIRETIME = 604800;
+
+/**
+ * 仅仅for test
+ */
+static const int testSDKAppID = 0;
+static const char *testUserSig = "";
+
 
 #endif  // QTMACDEMO_BASE_DEFS_H_
