edb7c7f7d7860d256570d2bab2d865a56aeabf35
gavinwjwang <gavinwjwang@tencent.com>
2021-12-08 11:57:25 +0800

    TencentPlugin

--- TRTC_Demo/Plugins/TencentPlugin/Source/TencentPlugin/TencentPlugin.Build.cs
+++ TRTC_Demo/Plugins/TencentPlugin/Source/TencentPlugin/TencentPlugin.Build.cs
@@ -43,6 +43,53 @@ public class TencentPlugin : ModuleRules
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]{}
 		);
-		
+		string _TRTCSDKPath =  Path.GetFullPath(Path.Combine(ModuleDirectory, "../ThirdParty/TRTCSDK"));; // ""
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+		if (Target.Platform == UnrealTargetPlatform.Android)
+		{
+				
+		}else if (Target.Platform == UnrealTargetPlatform.IOS)
+		{
+			// PublicAdditionalLibraries.AddRange(new string[] {
+			// 		"resolv",
+			// 		"z",
+			// 		"c++",
+			// });
+			// PublicFrameworks.AddRange(
+			// 		new string[] {
+			// 				"CoreML",
+			// 				"VideoToolbox",
+			// 				"Accelerate",
+			// 				"CFNetwork",
+			// 				"OpenGLES",
+			// 				"AVFoundation",
+			// 				"CoreTelephony"
+			// 		}
+			// );
+			PublicAdditionalFrameworks.Add(new UEBuildFramework( "TXLiteAVSDK_TRTC",_TRTCSDKPath+"/ios/TXLiteAVSDK_TRTC.framework.zip", ""));
+		}else if(Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			PublicAdditionalLibraries.AddRange(new string[] {
+				"resolv",
+				"z",
+				"c++",
+				"bz2",
+			});
+			PublicFrameworks.Add(Path.Combine(_TRTCSDKPath, "Mac", "Release","TXLiteAVSDK_TRTC_Mac.framework"));
+		}else if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			PublicAdditionalLibraries.Add(Path.Combine(_TRTCSDKPath, "win64", "Release","liteav.lib"));
+			PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "liteav.dll"));
+			PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "LiteAvAudioHook.dll"));
+			PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "LiteAvAudioHookService.dll"));
+			PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "openh264.dll"));
+			PublicDelayLoadDLLs.Add(Path.Combine(_TRTCSDKPath, "win64", "Release", "TRAE.dll"));
+
+			RuntimeDependencies.Add("$(BinaryOutputDir)/liteav.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "liteav.dll"));
+			RuntimeDependencies.Add("$(BinaryOutputDir)/LiteAvAudioHook.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "LiteAvAudioHook.dll"));
+			RuntimeDependencies.Add("$(BinaryOutputDir)/LiteAvAudioHookService.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "LiteAvAudioHookService.dll"));
+			RuntimeDependencies.Add("$(BinaryOutputDir)/openh264.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "openh264.dll"));
+			RuntimeDependencies.Add("$(BinaryOutputDir)/TRAE.dll", Path.Combine(_TRTCSDKPath, "win64", "Release", "TRAE.dll"));
+		}
 	}
 }
